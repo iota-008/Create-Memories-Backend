80696168fb5fb544667fb6a1957e9895953cf3b8
Ankit Raibole <46680697+iota-008@users.noreply.github.com>
2023-06-17 22:11:50 +0000

    updates

--- index.js
+++ index.js
@@ -8,39 +8,40 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 const app = express();
 dotenv.config();
-app.use(cookieParser());
+app.use( cookieParser() );
 app.use(
-	bodyParser.json({
+	bodyParser.json( {
 		limit: "50mb",
 		extended: true,
-	})
+	} )
 );
 app.use(
-	bodyParser.urlencoded({
+	bodyParser.urlencoded( {
 		limit: "50mb",
 		extended: true,
-	})
+	} )
 );
 app.use(
-	cors({
-		origin: ["http://localhost:3000", "https://create-your-memory.netlify.app"],
-	})
+	cors( {
+		origin: ["http://127.0.0.1:3000", "https://create-your-memory.netlify.app"],
+	} )
 );
-app.use("/user", userRoutes);
-app.use("/posts", postRoutes);
+app.use( "/user", userRoutes );
+app.use( "/posts", postRoutes );
 
-app.get("/", (req, res) => {
-	res.send("Welcome to memories API");
-});
+app.get( "/", ( req, res ) =>
+{
+	res.send( "Welcome to memories API" );
+} );
 
 const PORT = process.env.PORT || 5000;
 mongoose
-	.connect(process.env.CONNECTION_URL, {
+	.connect( process.env.CONNECTION_URL, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 		useFindAndModify: false,
-	})
-	.then(() =>
-		app.listen(PORT, () => console.log(`server is runing on port ${PORT}`))
+	} )
+	.then( () =>
+		app.listen( PORT, () => console.log( `server is runing on port ${ PORT }` ) )
 	)
-	.catch((error) => console.log("error : ", error.message));
+	.catch( ( error ) => console.log( "error : ", error.message ) );
